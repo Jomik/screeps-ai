0ebdc2e4f1412bd2faffc0f02a70b7df7ea6ade5
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2022-03-13 20:04:49 +0100

    delete unused

--- src/utils/object.ts
+++
@@ -1,7 +0,0 @@
-type Entries<T> = {
-  [K in keyof T]: [K, T[K]];
-}[keyof T][];
-
-export const entries = <T extends Record<string, unknown>>(
-  obj: T
-): Entries<T> => Object.entries(obj) as never;
