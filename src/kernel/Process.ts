b732274f7128e588e91a33f55ac53ca7d1d798ad
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2022-03-25 13:51:33 +0100

    update augmented memory

--- src/kernel/Process.ts
+++ src/kernel/Process.ts
@@ -17,67 +17,49 @@ type Config<M extends ProcessMemory> = {
   logger: Logger;
 };
 
-type NamingConvention<S extends string> =
-  | `${string}${Capitalize<S>}`
-  | `${Lowercase<S>}${string}`;
-
 type AugmentedMemory<M extends ProcessMemory> = {
-  [Key in keyof M]: M[Key] extends string | undefined
-    ? Key extends NamingConvention<'room'>
-      ? M[Key] extends undefined
-        ? Room | undefined
-        : Room
-      : Key extends NamingConvention<'creep'>
-      ? M[Key] extends undefined
-        ? Creep | undefined
-        : Creep
-      : M[Key]
-    : M[Key];
+  [Key in keyof M as M[Key] extends Id<_HasId> | undefined
+    ? Key
+    : never]: M[Key] extends Id<_HasId> | undefined
+    ? fromId<NonNullable<M[Key]>> | null
+    : never;
 };
 
 export abstract class Process<M extends ProcessMemory = Record<string, never>> {
   protected readonly logger: Logger;
   private config: Config<M>;
-  protected memory: AugmentedMemory<M>;
+  protected objects: AugmentedMemory<M>;
 
   constructor(config: Config<M>) {
     this.logger = config.logger;
     this.config = config;
 
-    this.memory = new Proxy<Config<M>, AugmentedMemory<M>>(this.config, {
-      set(target, property: string, value: unknown) {
+    this.objects = new Proxy<Config<M>, AugmentedMemory<M>>(this.config, {
+      set(target, property: string, value: _HasId | undefined) {
         const memory = target.memory();
-        if (typeof value !== 'string' || value !== undefined) {
-          return Reflect.set(memory, property, value, memory);
-        }
-        if (property.startsWith('room') || property.endsWith('Room')) {
-          return Reflect.set(memory, property, (value as Room).name, memory);
-        }
-        if (property.startsWith('creep') || property.endsWith('Creep')) {
-          return Reflect.set(memory, property, (value as Creep).name, memory);
-        }
-        return Reflect.set(memory, property, value, memory);
+        return Reflect.set(memory, property, value?.id, memory);
       },
       get(target, property: string) {
         const memory = target.memory();
-        if (typeof memory[property] !== 'string') {
+        if (memory[property] === undefined || memory[property] === null) {
           return Reflect.get(memory, property, memory) as never;
         }
 
-        if (property.startsWith('room') || property.endsWith('Room')) {
-          return Game.rooms[memory[property] as never];
-        }
-        if (property.startsWith('creep') || property.endsWith('Creep')) {
-          return Game.creeps[memory[property] as never];
-        }
-
-        return Reflect.get(memory, property, memory) as never;
+        return Game.getObjectById(memory[property] as Id<_HasId>);
       },
     });
   }
 
+  protected get memory(): M {
+    return this.config.memory();
+  }
+
   protected get children(): ChildDescriptor[] {
     return this.config.children();
+  }
+
+  protected hasChildOfType(type: ProcessConstructor<any>): boolean {
+    return this.children.some((v) => v.type === type);
   }
 
   abstract run(): Thread;
