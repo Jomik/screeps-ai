41808be706a76c412c1012d57cc5e0aac9a215e1
Jonas Holst Damtoft <Jomik@users.noreply.github.com>
2022-11-24 15:35:44 +0100

    get actual test coverage (#7)

--- packages/coroutines/jest.config.js
+++ packages/coroutines/jest.config.js
@@ -3,6 +3,7 @@ const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   coverageReporters: [['lcov', { projectRoot: '../..' }]],
+  collectCoverageFrom: ['src/**/*.ts'],
 };
 
 module.exports = config;
