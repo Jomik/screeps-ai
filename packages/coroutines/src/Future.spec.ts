819665453731e3741d76a57ded8a7b9cdbc1c011
Jonas Holst Damtoft <Jomik@users.noreply.github.com>
2023-09-26 18:32:47 +0200

    refactor(future): use defer in tests

--- packages/coroutines/src/Future.spec.ts
+++ packages/coroutines/src/Future.spec.ts
@@ -4,11 +4,8 @@ describe('Future', () => {
   it('calls then', () => {
     let res = '';
     const expected = 'foo';
-    const resolver = jest.fn();
+    const [uut, resolver] = Future.defer<string>();
 
-    const uut = new Future<string>((resolve) =>
-      resolver.mockImplementation(resolve)
-    );
     uut.then((value) => (res = value));
     resolver(expected);
 
@@ -18,11 +15,8 @@ describe('Future', () => {
   it('calls then if already resolved', () => {
     let res = '';
     const expected = 'foo';
-    const resolver = jest.fn();
+    const [uut, resolver] = Future.defer<string>();
 
-    const uut = new Future<string>((resolve) =>
-      resolver.mockImplementation(resolve)
-    );
     resolver(expected);
     uut.then((value) => (res = value));
 
